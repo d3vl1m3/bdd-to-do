ce276664b357032619ecba48e2c597bc20c81bc5
d3vl1m3 <dev_lime@protonmail.com>
2020-04-23 22:37:18 +0100

    - added form to sort tasks by how important/urgent they are - wrote tests for features

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,94 @@
     <div id="app">
         <div class="container">
             <div class="row-cols-1">
-                Welcome to app
+                <h1>What needs sorting?</h1>
                 <form @submit.prevent="addItem">
                     <input type="text"
                            class="new-task-input"
                            v-model="task">
-                    <button class="new-task-submit" type="submit">Add</button>
+                    <button class="new-task-submit"
+                            type="submit">
+                        Add
+                    </button>
+                    <button class="task-sort-trigger"
+                            type="button"
+                            @click="beginSorting()"
+                            :disabled="!tasks.length">
+                        <template v-if="sorting">
+                            Redo!
+                        </template>
+                        <template v-else>
+                            Begin!
+                        </template>
+                    </button>
                 </form>
 
-                <ul class="task-list" v-if="tasks.length">
-                    <li v-for="(task, i) in tasks"
-                        :key="i">
-                        {{ task }}
-                        <font-awesome-icon icon="times-circle" class="task-item-close" @click="removeItem(i)"/>
-                    </li>
-                </ul>
+                <template v-if="sorting && tasks.length">
+                    <form class="task-sorter"
+                          @submit.prevent>
+                        <h2>How urgent/important is each task?</h2>
+                        <div v-for="(task, i) in tasks"
+                             :key="i"
+                             class="sorting-task-question-container">
+                            <h3>{{ task }}</h3>
+                            <div class="container">
+                                <div class="row row-cols-2">
+                                    <div>
+                                        <fieldset>
+                                            <legend>How important?</legend>
+                                            <div class="container">
+                                                <div class="row row-cols-5">
+                                                    <div v-for="n in 5"
+                                                         :key="n"
+                                                         class="form-check">
+                                                        <input type="radio"
+                                                               class="form-check-input"
+                                                               :id="`important_${i}_${n}`"
+                                                               :name="`important_${i}`">
+                                                        <label class="form-check-label d-block"
+                                                                :for="`important_${i}_${n}`">{{ n }}</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </div>
+                                    <div>
+                                        <fieldset>
+                                            <legend>How urgent?</legend>
+                                            <div class="container">
+                                                <div class="row row-cols-5">
+                                                    <div v-for="n in 5"
+                                                        :key="n"
+                                                        class="form-check">
+                                                        <input type="radio"
+                                                               class="form-check-input"
+                                                               :id="`urgency_${i}_${n}`"
+                                                               :name="`urgency_${i}`">
+                                                        <label class="form-check-label d-block"
+                                                                :for="`urgency_${i}_${n}`">{{ n }}</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary my-2">Done!</button>
+                    </form>
+                </template>
+                <template v-else-if="tasks.length && !sorting">
+                    <ul class="task-list" >
+                        <li v-for="(task, i) in tasks"
+                            :key="i">
+                            {{ task }}
+                            <font-awesome-icon
+                                    icon="times-circle"
+                                    class="task-item-close"
+                                    @click="removeItem(i)"/>
+                        </li>
+                    </ul>
+                </template>
             </div>
         </div>
     </div>
@@ -28,9 +101,14 @@
             hide: false,
             task: '',
             tasks: [],
+            sorting: false
         }),
         methods: {
             addItem() {
+                if ( this.sorting) {
+                    this.sorting = false;
+                }
+
                 if (this.task.trim() !== '') {
                     this.tasks.push(this.task);
                     this.task = '';
@@ -40,6 +118,9 @@
                 if (this.tasks[i]) { // simple check to make sure it exists 🤷‍
                     this.tasks.splice(i, 1);
                 }
+            },
+            beginSorting() {
+                this.sorting = true;
             }
         }
     }
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,73 +1,121 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-function aliasTakList() {
+function aliasTaskList() {
   cy.get('.task-list').as('taskList');
 }
 
-describe('Testing todo list', () => {
+function initTaskListWithItems() {
+  // button should always be disabled to start
+  cy.get('@taskSortTrigger').should('be.disabled');
 
+  // type out all the tasks required
+  cy.get('@newTaskInput')
+      .type('A test task 1{enter}')
+      .type('{enter}') // should not add anything
+      .type('null{enter}')
+      .type('Zero{enter}')
+      .type('false'); // should not add anything
+
+  // add the last item via button click instead
+  cy.get('@newTaskSubmit').click();
+
+  aliasTaskList();
+
+}
+
+describe('Testing todo list', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.get('.new-task-input').as('newTaskInput');
     cy.get('.new-task-submit').as('newTaskSubmit');
+    cy.get('.task-sort-trigger').as('taskSortTrigger');
+    // alias for list now available
+    initTaskListWithItems();
   });
 
   it('Can add a task to a list', () => {
-    cy.visit('/');
-    cy.get('@newTaskInput')
-        .type('A test task 1{enter}');
-
-    aliasTakList();
-
-    cy.get('@taskList')
-        .children('li')
-        .should('have.length', 1);
-
-
-    cy.get('@newTaskInput')
-        .type('{enter}') // should not add anything
-        .type('null{enter}')
-        .type('0{enter}')
-        .type('false'); // should not add anything
-
-
-    cy.get('@newTaskSubmit').click();
-
+    // check the expected items are there
     cy.get('@taskList')
         .children('li')
         .should('have.length', 4);
+
   });
 
   it('Can remove a task from the list', () => {
-    cy.visit('/');
-
-    cy.get('@newTaskInput')
-        .type('A test task 1{enter}')
-        .type('{enter}') // should not add anything
-        .type('null{enter}')
-        .type('0{enter}')
-        .type('false'); // should not add anything
-
-    cy.get('.new-task-submit').click();
-
-    aliasTakList();
-
-    cy.get('@taskList')
-        .children('li')
-        .should('have.length', 4);
-
+    // click the delete icon on the 3rd item
     cy.get('@taskList')
         .children('li')
         .eq(2)
         .find('.task-item-close')
         .click();
 
+    // length is updated
     cy.get('@taskList')
         .children('li')
         .should('have.length', 3);
 
+    // item containing 0 no longer exists
     cy.get('@taskList')
         .children('li')
-        .should('not.contain', '0'); // this was at index pos 2
-  })
+        .should('not.contain', '0');
+  });
+
+  it('The user can activate a prioritising event', () => {
+    // a button for 'begin!' is no longer disabled
+    cy.get('@taskSortTrigger')
+        .should('not.be.disabled')
+        // now lets trigger the sorting form to appear
+        .click();
+
+    // a form is now available
+    cy.get('form.task-sorter')
+        .as('taskSorter');
+
+    // which asks the urgency/importance of each task
+    cy.get('@taskSorter')
+        .children('.sorting-task-question-container')
+        .should('have.length', 4);
+
+    // all "important" fields are there
+    cy.get('@taskSorter')
+        .find('fieldset')
+        .eq(0) // grab the first fieldset
+        .find('input[id^=important_]')
+        .should('have.length', 5);
+
+    // all "urgent" fields are there
+    cy.get('@taskSorter')
+        .find('fieldset')
+        .eq(1) // grab the first fieldset
+        .find('input[id^=urgency_]')
+        .should('have.length', 5);
+  });
+
+  it('If a user adds a new task whilst prioritising, the task list replaces the form again', () => {
+    // now lets trigger the sorting form to appear
+    cy.get('@taskSortTrigger')
+        .click();
+
+    // a form is now available
+    cy.get('form.task-sorter')
+        .as('taskSorter');
+
+    cy.get('@taskSorter')
+        .should('exist');
+
+    cy.get('@newTaskInput')
+        .type('A test task 5{enter}');
+
+    cy.get('@taskSorter')
+        .should('not.exist');
+
+    cy.get('@taskList')
+        .should('exist')
+        .children('li')
+        .should('have.length', 5);
+  });
+
+  // it('When the user has done the sorting task, items are displayed in the correct order', () => {
+  //
+  // });
 });
